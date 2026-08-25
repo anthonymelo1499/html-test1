e5faf8979c90124be0da609c477f20037ff4264f
anthonymelo1499 <anthony.melo886@gmail.com>
2026-08-25 10:28:46 -0400

    Add files via upload

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,122 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "shorttitlelowercase-2",
+  "level": "1",
+  "url": "shorttitlelowercase-2.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Otto Baier Department of Mathematics Purdue University      "
+},
+{
+  "id": "lesson30",
+  "level": "1",
+  "url": "lesson30.html",
+  "type": "Section",
+  "number": "",
+  "title": "Lesson 30 - Trigonometric Equations III",
+  "body": " Lesson 30 - Trigonometric Equations III   Reminders:   Webassign HW29 due tonight  Written HW9 due Friday    Today's Lesson:   Section 7.5 (Precalculus)  Webassign HW30 (due Wednesday)      Main Idea  Our main goal today is to solve trigonometric equations that have a horizontal stretch or compression built into them. So far, we have solved equations like or but now we will solve equations like or Notice that in the former two examples, the input variable ( or ) has no scaling factor on it (or said another way, the scaling factor is ), whereas in the latter two examples, we have introduced a horizontal stretch or compression by adding a scaling factor to the input variable.    General Solutions    Find the general solution to the following equations. Give your answers as the smallest solution greater than plus a multiple of .     "
+},
+{
+  "id": "subsection-L30_2-2",
+  "level": "2",
+  "url": "lesson30.html#subsection-L30_2-2",
+  "type": "Example",
+  "number": "1",
+  "title": "",
+  "body": "  Find the general solution to the following equations. Give your answers as the smallest solution greater than plus a multiple of .   "
+},
+{
+  "id": "lesson31",
+  "level": "1",
+  "url": "lesson31.html",
+  "type": "Section",
+  "number": "",
+  "title": "Lesson 31 - Applications of Trigonometry",
+  "body": " Lesson 31 - Applications of Trigonometry   Reminders:   Webassign HW30 due tonight  Written HW9 due Friday    Today's Lesson:   Section 5.4, 6.3, 7.5 (Precalculus)  Webassign HW31 (due Friday)      Application Problems  The angle of elevation or angle of inclination is the angle formed between a line that extends above the horizontal and the horizontal plane:   An angle is created by two line segments which share an endpoint. One line segment is horizontal, and the other line segment has an endpoint above the horizontal. The acute angle formed by these line segments is labeled as the angle of elevation.    The angle of depression is the angle formed between a line that extends below the horizontal and the horizontal plane:   An angle is created by two line segments which share an endpoint. One line segment is horizontal, and the other line segment has an endpoint below the horizontal. The acute angle formed by these line segments is labeled as the angle of depression.    For these examples, it is important that we identify what information is given and what information the question is asking for. The best way to do this is often drawing a picture. By sketching out a picture of the scenario described in a word problem, we can more easily find our way to the solution while avoiding common errors such as misidentifying a side or angle of a triangle or using the wrong trigonometric ratio. Finally, our picture can help us double check if we've made any mistakes by asking ourselves \"does the answer I got make sense for this picture\"?    In the design of a new building, a doorway is 2.65 ft above the ground. A ramp, at an angle of 4° with the ground, is to be built up to the doorway. How long will the ramp be? Round your answer to 3 decimal places.    37.989ft    First, draw a picture to identify which information in this problem is given and which information the question is asking for:   A diagram depicting a right triangle with a horizontal side at ground level and a vertical side that is labeled as 2.65ft long, upon which the door sits. The hypotenuse of the right triangle is labeled as \"ans\". The acute angle formed by the hypotenuse and horizontal is labeled as \"4°\".   After labeling the given information and what we are trying to solve for, we can ask ourselves: \"if I know the angle and opposite side, how can I find the hypotenuse?\" Using SOH-CAH-TOA we know that so we have the equation where represents the length of the hypotenuse in feet. We can solve this equation (with the help of our calculator) by first multiplying both sides by and then dividing both sides by to get   Finally, we can do a \"reality check\" on the answer we arrived at. It should make sense that the base of the triangle is almost 38 feet, as an angle of 4° is very small, so the base must be much longer than the height. If we had drawn our diagram to scale (i.e. with a ruler and compass so that the proportions are accurate), the right triangle would look like this:   A diagram of a right triangle with a height of 2.65 feet and an angle of 4°. It is drawn to scale so that the horizontal side is dramatically longer than the height.    If, for example, we accidentally used radians on our calculator, we would have gotten —this doesn't make sense as not only can a length not be negative, but 3.5 feet is too short to create a 4° angle with a height of 2.65 feet.      When the elevation of the sun is 38°, a tree casts a shadow 26.5 feet long. How tall is the tree? Round to one decimal.    20.7ft    First, draw a picture:   A diagram depicting         Non-right Triangles  blah   "
+},
+{
+  "id": "subsec-L31_1-2",
+  "level": "2",
+  "url": "lesson31.html#subsec-L31_1-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "angle of elevation angle of inclination "
+},
+{
+  "id": "subsec-L31_1-4",
+  "level": "2",
+  "url": "lesson31.html#subsec-L31_1-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "angle of depression "
+},
+{
+  "id": "subsec-L31_1-7",
+  "level": "2",
+  "url": "lesson31.html#subsec-L31_1-7",
+  "type": "Example",
+  "number": "1",
+  "title": "",
+  "body": "  In the design of a new building, a doorway is 2.65 ft above the ground. A ramp, at an angle of 4° with the ground, is to be built up to the doorway. How long will the ramp be? Round your answer to 3 decimal places.    37.989ft    First, draw a picture to identify which information in this problem is given and which information the question is asking for:   A diagram depicting a right triangle with a horizontal side at ground level and a vertical side that is labeled as 2.65ft long, upon which the door sits. The hypotenuse of the right triangle is labeled as \"ans\". The acute angle formed by the hypotenuse and horizontal is labeled as \"4°\".   After labeling the given information and what we are trying to solve for, we can ask ourselves: \"if I know the angle and opposite side, how can I find the hypotenuse?\" Using SOH-CAH-TOA we know that so we have the equation where represents the length of the hypotenuse in feet. We can solve this equation (with the help of our calculator) by first multiplying both sides by and then dividing both sides by to get   Finally, we can do a \"reality check\" on the answer we arrived at. It should make sense that the base of the triangle is almost 38 feet, as an angle of 4° is very small, so the base must be much longer than the height. If we had drawn our diagram to scale (i.e. with a ruler and compass so that the proportions are accurate), the right triangle would look like this:   A diagram of a right triangle with a height of 2.65 feet and an angle of 4°. It is drawn to scale so that the horizontal side is dramatically longer than the height.    If, for example, we accidentally used radians on our calculator, we would have gotten —this doesn't make sense as not only can a length not be negative, but 3.5 feet is too short to create a 4° angle with a height of 2.65 feet.   "
+},
+{
+  "id": "subsec-L31_1-8",
+  "level": "2",
+  "url": "lesson31.html#subsec-L31_1-8",
+  "type": "Example",
+  "number": "2",
+  "title": "",
+  "body": "  When the elevation of the sun is 38°, a tree casts a shadow 26.5 feet long. How tall is the tree? Round to one decimal.    20.7ft    First, draw a picture:   A diagram depicting      "
+},
+{
+  "id": "section-2",
+  "level": "1",
+  "url": "section-2.html",
+  "type": "Section",
+  "number": "",
+  "title": "My First Section",
+  "body": " My First Section   Some words    My First Subsection  Some more words  foo   A frog   A nice-looking frog with a longish description.    Bar:   "
+},
+{
+  "id": "subsection-2_1-4",
+  "level": "2",
+  "url": "section-2.html#subsection-2_1-4",
+  "type": "Figure",
+  "number": "1",
+  "title": "",
+  "body": " A frog   A nice-looking frog with a longish description.   "
+},
+{
+  "id": "section-3",
+  "level": "1",
+  "url": "section-3.html",
+  "type": "Section",
+  "number": "",
+  "title": "My Additional Section",
+  "body": " My Additional Section  foo   The graph made by TikZ   A 5-cycle     bar  "
+},
+{
+  "id": "figure-tikz-example-diagram",
+  "level": "2",
+  "url": "section-3.html#figure-tikz-example-diagram",
+  "type": "Figure",
+  "number": "1",
+  "title": "",
+  "body": " The graph made by TikZ   A 5-cycle    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
